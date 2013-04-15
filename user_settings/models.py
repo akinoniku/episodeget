0174c2fc00383b37cb5826c49440a5188f3e2473
akinoniku <akinoniku@gmail.com>
2013-04-15 18:33:07 +0800

    lixian add task

--- user_settings/models.py
+++ user_settings/models.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
+from xunlei.lixian import XunleiClient
+
 
 class Xunlei(models.Model):
     user = models.ForeignKey(User, blank=True, null=True)
     xunlei_id = models.CharField(max_length=300, blank=True, null=True)
     xunlei_pass = models.CharField(max_length=300, blank=True, null=True)
     cookies = models.CharField(max_length=6000, blank=True, null=True)
+
+    def add_task(self, rss):
+        xunlei = XunleiClient(username=self.xunlei_id, password=self.xunlei_pass, cookie_path=self.xunlei_id)
+        xunlei.add_torrent_task_by_link(rss.link)
 
 
 class SubListPrefer(models.Model):
