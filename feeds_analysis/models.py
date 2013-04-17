ebf0c4f0c6c59d96823e966849fd6cbbc2e3be57
akinoniku <akinoniku@gmail.com>
2013-04-17 18:30:38 +0800

    add a signal for new rss (not working for now)

--- feeds_analysis/models.py
+++ feeds_analysis/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from extra_app.langcov import langconv
+from feeds_analysis.analysiser import analysis_tags
 
 STYLE_CHOICES = (
     ('TM', '字幕组'),
@@ -221,3 +222,18 @@ def update_with_id(instance, **kwargs):
 
 
 post_save.connect(update_with_id, sender=Douban)
+
+
+def analysis_tags_with_rss(instance, **kwargs):
+    """
+    Analysis after get the new rss
+    :param instance:
+    :param kwargs:
+    """
+    if None:
+        analysis_tags(instance)
+        #TODO then send the notification to user email
+
+
+post_save.connect(analysis_tags_with_rss, sender=Rss)
+
