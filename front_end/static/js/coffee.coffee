0a3e049127bc41c0e25e4fec7944725b96493415
akinonoku <akinoniku@gmail.com>
2013-04-29 00:00:03 +0800

    homepage js update

--- front_end/static/js/coffee.coffee
+++ front_end/static/js/coffee.coffee
@@ -1,3 +1,12 @@
 $('.home-hero').height $(window).height()-4
 $(window).resize ->
   $('.home-hero').height $(window).height()-4
+$(window).scroll ->
+  if $(document).scrollTop()> $(window).height()*3/5
+    $('.feature-item-1').fadeIn(1400)
+  if $(document).scrollTop()> $(window).height()*3/5+20
+    $('.feature-item-2').fadeIn(1400)
+  if $(document).scrollTop()> $(window).height()*3/5+40
+    $('.feature-item-3').fadeIn(1400)
+  if $(document).scrollTop()> $(window).height()*3/5
+   $('#time-line').animate 'margin-top': '30px'
--- front_end/static/js/coffee.js
+++ front_end/static/js/coffee.js
@@ -6,4 +6,21 @@
     return $('.home-hero').height($(window).height() - 4);
   });
 
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > $(window).height() * 3 / 5) {
+      $('.feature-item-1').fadeIn(1400);
+    }
+    if ($(document).scrollTop() > $(window).height() * 3 / 5 + 20) {
+      $('.feature-item-2').fadeIn(1400);
+    }
+    if ($(document).scrollTop() > $(window).height() * 3 / 5 + 40) {
+      $('.feature-item-3').fadeIn(1400);
+    }
+    if ($(document).scrollTop() > $(window).height() * 3 / 5) {
+      return $('#time-line').animate({
+        'margin-top': '30px'
+      });
+    }
+  });
+
 }).call(this);
