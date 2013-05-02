4f9f99778d4951d54df3bff41d672d4bd9b607cc
akinoniku <akinoniku@gmail.com>
2013-05-02 18:20:43 +0800

    info intro

--- front_end/views.py
+++ front_end/views.py
@@ -1,12 +1,20 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template import Context
+from feeds_analysis.models import Info
 
 
 def index(request):
-    return render_to_response('front_end/index.html')
+    info_list = []
+    for info_id in range(15, 23):
+        info_list.append(get_info_show(info_id))
+    return render_to_response('front_end/index.html', {'info_list': info_list})
 
 
 def index_manifest(request):
     return HttpResponse("nothing")
     # return render_to_response('front_end/index.manifest', content_type='text/cache-manifest')
+
+def get_info_show(info_id):
+    return Info.objects.select_related().get(pk=info_id)
