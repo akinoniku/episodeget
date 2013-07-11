242854dab9577fa0a285915d0b58a734999a5ece
akinoniku <akinoniku@gmail.com>
2013-07-11 11:53:01 +0800

    save prefer and get prefer

--- user_settings/views.py
+++ user_settings/views.py
@@ -7,23 +7,27 @@ from user_settings.models import SubListPrefer
 def notify_user(user, rss):
     pass
 
+
 @api_view(['POST'])
 def save_prefer(request):
     user = request.user
     prefer_list = request.POST['list']
-    prefer = SubListPrefer.objects.get(user=user)
-    if prefer.id:
-        prefer.preferList = prefer_list
-        prefer.save()
-    else:
-        prefer = SubListPrefer(
-            user=user,
-            preferList=prefer_list
-        )
-        prefer.save()
-    Response(data=True)
+    try:
+        prefer = SubListPrefer.objects.get(user=user)
+    except:
+        prefer = SubListPrefer(user=user)
+
+    prefer.preferList = prefer_list
+    prefer.save()
+    return Response(data=True)
 
 
-@api_view(['POST'])
+@api_view(['GET'])
 def get_prefer(request):
     user = request.user
+    try:
+        prefer = SubListPrefer.objects.get(user=user)
+        data = prefer.preferList
+    except:
+        data = False
+    return Response(data=data)
